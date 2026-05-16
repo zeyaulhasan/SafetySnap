f5c705a446e598339c6e4a073d836ca790fed14a
Zeyaul Hasan <Zeyaulhasan1326@gmail.com>
2026-05-17 01:21:52 +0530

    Fix Mongoose async next bug in User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -40,11 +40,10 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare password helper
